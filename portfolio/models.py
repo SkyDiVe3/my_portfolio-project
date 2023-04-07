db81b54e78582c7aac21fd3ab7b0b8aaf34cfcc5
Onica Mihail <onicamihail2002@gmail.com>
2023-04-07 14:04:29 +0300

    Started Project
    1.Created project 
    2.Created 'Blog' and 'Portfolio' apps and added them to the settings 
    3.Created a model for Portfolio app and migrated it with all other project migrations
    4.Initialized git and created a .gitignore for django,pycharm,Intellije

---
+++ portfolio/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here. A model is tippicaly a class of an database table witch
+# have rows and every row has properties
+class Project(models.Model):
+    title = models.CharField(max_length = 100)
+    description = models.CharField(max_length = 250)
+    blogImage = models.ImageField(upload_to='portfolio/blogImages/')
+    url = models.URLField(blank=True)
