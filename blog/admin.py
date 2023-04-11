126c983f5c8a5887b8134490a95e24318e736df4
Onica Mihail <onicamihail2002@gmail.com>
2023-04-11 20:06:17 +0300

    Blog's Model Test
    1.Created a testing model for blogs and registered it to admin
    2. Created a function in views for  calling elements from model, and ordering/or limiting them.
    3.In the existing template from blogs we visualized them

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Blog
 
-# Register your models here.
+#Registering a model means u approve to show it in admin panel
+admin.site.register(Blog)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+    blogs = Blog.objects.all();
+    # If u want to order/limit the showing of the elements from model u can:
+    # blogs = Blog.objects.order_by(-date)[:5];
+    # to order by date, so the newest 5 elements will pop up on top
+
+
+    return render(request, 'blog/all_blogs.html', {'blogs': blogs})
